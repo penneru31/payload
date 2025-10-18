b4dbb132710a06984a77fd9d7c663cb8773c4663
Rishitha Penneru <pennerurishitha31@gmail.com>
2025-10-18 15:58:38 +0530

    fix: adjust OpenNext config for Cloudflare

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,4 +1,14 @@
-// default open-next.config.ts file created by @opennextjs/cloudflare
+// open-next.config.ts
+
 import { defineCloudflareConfig } from '@opennextjs/cloudflare/config'
 
-export default defineCloudflareConfig({})
+export default defineCloudflareConfig({
+  // @ts-expect-error: Temporarily unsupported in type definitions
+  functions: {
+    default: {
+      memory: 512,
+      maxDuration: 10,
+      runtime: 'nodejs18.x',
+    },
+  },
+})
